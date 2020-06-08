c3794a940bc423acff366a234ce8b67892969f6c
Anni-HSE <55951421+Anni-HSE@users.noreply.github.com>
2020-06-09 01:36:01 +0500

    Add Task8

---
+++ Tas8_v19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tas8_v19
+{
+    public class Program
+    {
+        static int SizeInput()
+        {
+            int number;
+            Console.WriteLine("Введите размер матрицы смежности");
+
+            while(!(int.TryParse(Console.ReadLine(), out number)) || number < 0)
+            {
+
+            }
+
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Tas8_v19/obj/Debug/netcoreapp3.1/Tas8_v19.AssemblyInfo.cs
+++ Tas8_v19/obj/Debug/netcoreapp3.1/Tas8_v19.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Tas8_v19")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Practice")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
+[assembly: System.Reflection.AssemblyProductAttribute("Tas8_v19")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Tas8_v19")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
